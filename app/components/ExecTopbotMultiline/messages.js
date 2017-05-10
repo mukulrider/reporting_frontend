a927bf3ea12ce2800c75b38a625596b67437bd59
musigma <padmapriya.p@mu-sigma.com>
2017-05-10 13:49:01 +0530

    Exec 85% completed

---
+++ app/components/ExecTopbotMultiline/messages.js
@@ -0,0 +1,13 @@
+/*
+ * ExecTopbotMultiline Messages
+ *
+ * This contains all the text for the ExecTopbotMultiline component.
+ */
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  header: {
+    id: 'app.components.ExecTopbotMultiline.header',
+    defaultMessage: 'This is the ExecTopbotMultiline component !',
+  },
+});
